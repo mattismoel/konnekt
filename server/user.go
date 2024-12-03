693842d14b28af345b1c53c0ae1fd7b7e859fb87
Mattis Kristensen <mattismoel@gmail.com>
2024-12-03 21:36:57 +0100

    remove user service interface

--- server/user.go
+++ server/user.go
@@ -1,22 +1,17 @@
 package konnekt
 
-import "context"
+import (
+	"net/mail"
+	"strings"
+
+	"github.com/mattismoel/konnekt"
+)
 
 type User struct {
 	ID        int64  `json:"id"`
 	Email     string `json:"email"`
 	FirstName string `json:"firstName"`
 	LastName  string `json:"lastName"`
-}
-
-type UserService interface {
-	FindUserByID(context.Context, int64) (User, error)
-	FindUsers(context.Context, UserFilter) ([]User, error)
-
-	CreateUser(context.Context, User) (int64, error)
-
-	UpdateUser(context.Context, UpdateUser) (User, error)
-	DeleteUser(context.Context, int64) error
 }
 
 type UserFilter struct {
