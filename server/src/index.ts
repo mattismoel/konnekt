be2653406d40bbc744ba382ab3c1e1027a2d1a62
Mattis Kristensen <mattismoel@gmail.com>
2024-12-07 16:58:57 +0100

    add logger middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,13 @@ import express from "express"
 import routes from "@route/routes"
 import { env } from "@/config/env";
 import error from "./middleware/error";
+import loggerMiddleware, { ConsoleLogger } from "./middleware/logger";
+
+const logger = new ConsoleLogger()
 
 const app = express()
 
+app.use(loggerMiddleware(logger))
 app.use(express.json())
 
 app.use(routes);
