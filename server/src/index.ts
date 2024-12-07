19640cba5a5870e8354959a30fd4367050bba516
Mattis Kristensen <mattismoel@gmail.com>
2024-12-07 15:56:50 +0100

    add error handling middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express"
 import { env } from "@/config/env";
+import error from "./middleware/error";
 const app = express()
 
 app.use(express.json())
+app.use(error)
+
 app.listen(env.PORT)
