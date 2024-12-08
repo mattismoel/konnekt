22b6a3819504ac369554cf80af2d97c7c166a547
Mattis Kristensen <mattismoel@gmail.com>
2024-12-09 00:37:17 +0100

    add event grid

---
+++ frontend/app/components/events/event-grid.tsx
@@ -0,0 +1,16 @@
+import { EventDTO } from "~/lib/event/event.dto"
+import { EventCard } from "./event-card"
+import { cn } from "~/lib/utils";
+
+type Props = {
+  events: EventDTO[]
+  className?: string;
+}
+
+export const EventGrid = ({ events, className }: Props) => {
+  return (
+    <div className={cn("grid grid-cols-1 md:grid-cols-2 gap-4", className)}>
+      {events.map(event => <EventCard key={event.id} event={event} />)}
+    </div>
+  )
+}
