9c4e1effe6519d24c76240c9e9839f35f7a2580a
Mattis Kristensen <mattismoel@gmail.com>
2024-12-11 21:17:35 +0100

    remove user greeting on dashboard

--- frontend/app/routes/admin.dashboard/route.tsx
+++ frontend/app/routes/admin.dashboard/route.tsx
@@ -1,18 +1,15 @@
 import { Outlet } from "@remix-run/react";
 import { Sidebar } from "./sidebar";
-import { useUser } from "@/lib/context/user.provider";
 
 const DashboardLayout = () => {
-    const { user } = useUser()
-    return (
-        <main className="min-h-sub-nav flex flex-col px-16 py-16">
-            <h3 className="font-bold text-2xl mb-4">Godaften, {user?.firstName}.</h3>
-            <div className="flex-1 grid grid-rows-1 grid-cols-[256px_1fr] gap-4">
-                <Sidebar />
-                <Outlet />
-            </div>
-        </main>
-    )
+  return (
+    <main className="min-h-sub-nav flex flex-col px-16 py-16">
+      <div className="flex-1 grid grid-rows-1 grid-cols-[256px_1fr] gap-4">
+        <Sidebar />
+        <Outlet />
+      </div>
+    </main>
+  )
 }
 
 export default DashboardLayout;
