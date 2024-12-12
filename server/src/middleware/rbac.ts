46e207c200f28e564d100479e6f3800950bcb123
Mattis Kristensen <mattismoel@gmail.com>
2024-12-12 18:41:43 +0100

    create middleware factory

--- server/src/middleware/rbac.ts
+++ server/src/middleware/rbac.ts
@@ -3,40 +3,40 @@ import type { RoleService } from "@/service/role.service";
 import { SESSION_COOKIE_NAME } from "@/shared/auth/constant";
 import type { RequestHandler } from "express";
 
-export const checkPermissions = (
-  authService: AuthService,
-  roleService: RoleService,
-  permissions: string[],
-): RequestHandler => {
-  return async (req, res, next) => {
-    const token = req.cookies[SESSION_COOKIE_NAME] as string || null
+export type PermissionCheckerMiddleware = (permissions: string[]) => RequestHandler
 
-    if (!token) {
-      res.sendStatus(401)
-      return
+export const createPermissionChecker = (authService: AuthService, roleService: RoleService): PermissionCheckerMiddleware => {
+  return (permissions: string[]): RequestHandler => {
+    return async (req, res, next) => {
+      const token = req.cookies[SESSION_COOKIE_NAME] as string || null
+
+      if (!token) {
+        res.sendStatus(401)
+        return
+      }
+
+      const { session, user } = await authService.validateSessionToken(token)
+
+      if (!session) {
+        res.sendStatus(401)
+        return
+      }
+
+      const roles = await roleService.getUserRoles(user.id)
+
+      const rolesPermissions = await Promise.all(
+        roles.map((role) => roleService.getRolePermissions(role.id))
+      )
+
+      const userPermissions = rolesPermissions.flat().map(p => p.name)
+
+      if (!permissionsMatch(permissions, userPermissions)) {
+        res.sendStatus(401)
+        return
+      }
+
+      next()
     }
-
-    const { session, user } = await authService.validateSessionToken(token)
-
-    if (!session) {
-      res.sendStatus(401)
-      return
-    }
-
-    const roles = await roleService.getUserRoles(user.id)
-
-    const rolesPermissions = await Promise.all(
-      roles.map((role) => roleService.getRolePermissions(role.id))
-    )
-
-    const userPermissions = rolesPermissions.flat().map(p => p.name)
-
-    if (!permissionsMatch(permissions, userPermissions)) {
-      res.sendStatus(401)
-      return
-    }
-
-    next()
   }
 }
 
