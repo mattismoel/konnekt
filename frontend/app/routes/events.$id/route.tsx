32cd122e3f0d19ac98a7e62bed686d8e37e5bd70
Mattis Kristensen <mattismoel@gmail.com>
2024-12-13 23:22:59 +0100

    implement react query on event page

--- frontend/app/routes/events.$id/route.tsx
+++ frontend/app/routes/events.$id/route.tsx
@@ -6,59 +6,47 @@ import { EventDetails } from "./event-details";
 import { EventCaroussel } from "@/components/events/event-caroussel";
 import { useEffect, useState } from "react";
 import { fetchEventByID, fetchEvents } from "@/lib/event";
-import { endOfWeek, startOfDay, startOfWeek } from "date-fns";
+import { endOfWeek, startOfDay, startOfToday, startOfWeek } from "date-fns";
+import { useQueries, useQuery } from "@tanstack/react-query";
 
 const EventPage = () => {
   const { id } = useParams()
-  const [event, setEvent] = useState<EventDTO | undefined>(undefined)
-  const [weeklyEvents, setWeeklyEvents] = useState<EventDTO[]>([])
-  const [recommendedEvents, setRecommendedEvents] = useState<EventDTO[]>([])
 
-  const [loading, setLoading] = useState(true)
+  const { data: event } = useQuery({
+    queryKey: ["events", id],
+    queryFn: () => fetchEventByID(parseInt(id || "0"))
+  })
 
-  const handleFetchEvent = async () => {
-    setLoading(true)
+  const { data: recommendedEventsResult } = useQuery({
+    queryKey: ["events"],
+    queryFn: () => fetchEvents({
+      fromDate: startOfWeek(event?.fromDate || new Date()),
+      toDate: endOfWeek(event?.fromDate || new Date())
+    }),
+    enabled: !!event
+  })
 
-    const event = await fetchEventByID(parseInt(id || "1"))
-
-    if (!event) {
-      throw new Error("Could not load event")
-    }
-
-    const { events: weeklyEvents } = await fetchEvents({
-      fromDate: startOfWeek(event.fromDate),
-      toDate: endOfWeek(event.fromDate)
-    })
-
-    setWeeklyEvents(weeklyEvents)
-
-    const { events: recommendedEvents } = await fetchEvents({
+  const { isPending, error, data: weeklyEventsResult } = useQuery({
+    queryKey: ["events"],
+    queryFn: () => fetchEvents({
       limit: 5,
-      fromDate: startOfDay(new Date()),
-    })
+      fromDate: startOfToday()
+    }),
+    enabled: !!event
+  })
 
-    // Filter out the actual event from the recommended.
-    setRecommendedEvents(recommendedEvents.filter(e => e.id !== event.id))
-
-    setEvent(event)
-
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    handleFetchEvent()
-  }, [])
+  if (error) return "Error"
 
   return (
     <main className="min-h-sub-nav pb-16 bg-black text-white">
-      <EventDetails event={event} isLoading={loading} />
+      <EventDetails event={event || undefined} isLoading={isPending} />
       <article dangerouslySetInnerHTML={{ __html: event?.description || "" }} className="px-auto pt-20 pb-16 text-gray-400 prose prose-invert max-w-none">
       </article>
       <div className="px-auto">
         <h2 className="font-bold text-2xl">Billetten gælder også til.</h2>
-        <EventCalendar activeID={event?.id} events={weeklyEvents} className="h-64" />
+        <EventCalendar activeID={event?.id} events={weeklyEventsResult?.events || []} className="h-64" />
         <h1 className="font-bold text-4xl mb-12">Se også.</h1>
-        <EventCaroussel events={recommendedEvents} />
+        <EventCaroussel events={recommendedEventsResult?.events || []} />
       </div>
     </main>
   )
