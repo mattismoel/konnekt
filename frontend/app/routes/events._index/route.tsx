68cf3cc3e5f965f35f59b202c56a890d2a1a7cde
Mattis Kristensen <mattismoel@gmail.com>
2024-12-13 23:22:45 +0100

    implement react query on events page

--- frontend/app/routes/events._index/route.tsx
+++ frontend/app/routes/events._index/route.tsx
@@ -1,9 +1,8 @@
 import { EventGrid } from "@/components/events/event-grid"
-import { EventDTO } from "@/lib/dto/event.dto"
-import { useEffect, useState } from "react"
 import { fetchEvents } from "@/lib/event"
 import { Paginator } from "@/components/ui/paginator"
 import { useSearchParams } from "@remix-run/react"
+import { useQuery } from "@tanstack/react-query"
 
 const PAGE_SIZE = 6
 
@@ -11,34 +10,24 @@ const EventsPage = () => {
   const [searchParams, setSearchParams] = useSearchParams()
   const page = parseInt(searchParams.get("page") || "1", 10)
 
-  const [events, setEvents] = useState<EventDTO[]>([])
-  const [totalPageCount, setTotalPageCount] = useState(0)
-  const [loading, setLoading] = useState(true)
 
-  useEffect(() => {
-    const handleFetchEvents = async () => {
-      setLoading(true)
-      const { events, totalSize } = await fetchEvents({
-        page,
-        pageSize: PAGE_SIZE,
-      })
+  const { isPending, error, data } = useQuery({
+    queryKey: ["events"],
+    queryFn: () => fetchEvents({
+      page,
+      pageSize: PAGE_SIZE
+    }),
+  })
 
-      setEvents(events)
-      let pageCount = Math.floor(totalSize / PAGE_SIZE)
-      if (pageCount === 0) pageCount = 1
-      setTotalPageCount(pageCount)
-      setLoading(false)
-    }
+  if (error) return "Error"
 
-    handleFetchEvents()
-  }, [page])
 
   return (
     <main className="px-auto py-20 min-h-sub-nav">
       <h1 className="text-2xl font-bold mb-4">Kommende events.</h1>
-      <EventGrid className="mb-4" events={events} isLoading={loading} />
+      <EventGrid className="mb-4" events={data?.events || []} isLoading={isPending} />
       <Paginator
-        totalPages={totalPageCount}
+        totalPages={Math.ceil((data?.totalSize || 1) / PAGE_SIZE) || 1}
         currentPage={page}
         onSelect={(newPage) => setSearchParams({ page: newPage.toString() })}
       />
