fbc277c4e98d306f6e72c96056e154cbd6cc0c9c
Mattis Kristensen <mattismoel@gmail.com>
2024-12-13 20:49:34 +0100

    make genre fetching client side

--- frontend/app/lib/genre.ts
+++ frontend/app/lib/genre.ts
@@ -4,8 +4,11 @@ import env from "@/config/env"
 /**
  * @description Gets all genres and returns them as a string array.
  */
-export const fetchAllGenres = async (opts: RequestInit): Promise<string[]> => {
-  const res = await fetch(`${env.BACKEND_URL}/genres`, opts)
+export const fetchAllGenres = async (): Promise<string[]> => {
+  const res = await fetch(`${window.ENV.BACKEND_URL}/genres`, {
+    credentials: "include"
+  })
+
   if (!res.ok) {
     console.error(`Could not get genres: ${res.statusText}`)
     return []
