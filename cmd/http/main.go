8e5a4be8956854fe14d3292113d7b7af0abfc3f4
Mattis Kristensen <mattismoel@gmail.com>
2025-01-24 11:34:56 +0100

    main file setup

---
+++ cmd/http/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"log"
+	"net"
+	"strconv"
+	"time"
+
+	"github.com/mattismoel/konnekt/internal/server"
+	"github.com/mattismoel/konnekt/internal/service"
+	"github.com/mattismoel/konnekt/internal/storage/sqlite"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const (
+	MAX_STARTUP_DURATION = 10 * time.Second
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), MAX_STARTUP_DURATION)
+	defer cancel()
+
+	flag.Parse()
+
+	dbConnStr := flag.String("dbConnStr", "file:./local.db", "The database connection string")
+	host := flag.String("host", "localhost", "The host of the web server")
+	port := flag.Int("port", 3000, "The port of the web server")
+
+	db, err := sql.Open("sqlite3", *dbConnStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := db.PingContext(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	userRepo, err := sqlite.NewUserRepository(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	authRepo, err := sqlite.NewAuthRepository(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	authService, err := service.NewAuthService(userRepo, authRepo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	srv, err := server.New(
+		server.WithAddress(net.JoinHostPort(*host, strconv.Itoa(*port))),
+		server.WithAuthService(authService),
+	)
+
+	if err := srv.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
