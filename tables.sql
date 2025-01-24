08b4fb03a151605df93fc925a9a5e9037da04dc7
Mattis Kristensen <mattismoel@gmail.com>
2025-01-24 12:11:57 +0100

    add rbac tables

--- tables.sql
+++ tables.sql
@@ -11,3 +11,27 @@ CREATE TABLE session (
   user_id INTEGER NOT NULL,
   expires_at TIMESTAMP NOT NULL
 );
+
+CREATE TABLE role (
+  id INTEGER PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE users_roles (
+  user_id INTEGER NOT NULL,
+  role_id INTEGER NOT NULL,
+  PRIMARY KEY (user_id, role_id)
+);
+
+CREATE TABLE permission (
+  id INTEGER PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE roles_permissions (
+  role_id INTEGER NOT NULL,
+  permission_id INTEGER NOT NULL,
+  PRIMARY KEY (role_id, permission_id)
+);
