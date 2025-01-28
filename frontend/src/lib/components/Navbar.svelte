eb06805d392f6936051931304599a971d5f8ec9f
Mattis Kristensen <mattismoel@gmail.com>
2025-01-28 21:19:06 +0100

    add navbar

---
+++ frontend/src/lib/components/Navbar.svelte
@@ -0,0 +1,9 @@
+<nav class="h-nav bg-background text-text flex items-center justify-between px-8">
+	<h3 class="font-black">KONNEKT</h3>
+
+	<ul class="text-text/80 *:hover:text-text flex space-x-5 font-medium">
+		<li><a href="/events">Events</a></li>
+		<li><a href="/artists">Artister</a></li>
+		<li><a href="/sponsorer">Sponsorer</a></li>
+	</ul>
+</nav>
