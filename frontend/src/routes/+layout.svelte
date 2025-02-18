01ead968d3c311aaca4406feced95db9e4508f80
Mattis Kristensen <mattismoel@gmail.com>
2025-02-18 19:19:56 +0100

    move navbar to layout file
    >

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,8 +1,19 @@
-<script lang="ts">
-	import Navbar from '$lib/components/Navbar.svelte';
-	import '../app.css';
+<script>
 	let { children } = $props();
+	import '../app.css';
 </script>
 
-<Navbar />
+{@render navbar()}
 {@render children()}
+{#snippet navbar()}
+	<nav class="h-nav flex items-center justify-between border-b border-b-neutral-950 px-8">
+		<a href="/" class="font-black">KONNEKT&reg;</a>
+		<ul class="*:hover:text-text text-text/85 flex gap-6">
+			<li><a href="/events">Events</a></li>
+			<li><a href="/artists">Kunstnere</a></li>
+			<li><a href="/sponsorer">Sponsorer</a></li>
+			<li><a href="/om-os">Om os</a></li>
+		</ul>
+	</nav>
+{/snippet}
+
