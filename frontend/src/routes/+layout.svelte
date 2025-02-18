914e4c5e2a94422ba2e25c2442e4661fc6786a2d
Mattis Kristensen <mattismoel@gmail.com>
2025-02-18 19:20:41 +0100

    add footer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -5,6 +5,8 @@
 
 {@render navbar()}
 {@render children()}
+{@render footer()}
+
 {#snippet navbar()}
 	<nav class="h-nav flex items-center justify-between border-b border-b-neutral-950 px-8">
 		<a href="/" class="font-black">KONNEKT&reg;</a>
@@ -17,3 +19,32 @@
 	</nav>
 {/snippet}
 
+{#snippet footer()}
+	<footer class="px-auto border-t border-t-neutral-900 bg-neutral-950 pt-8 pb-6">
+		<div class="mb-8 grid w-full grid-cols-1 sm:grid-cols-2">
+			<!-- NAVIGATION -->
+			<div class="flex-1">
+				<h3 class="mb-2 font-bold">Find rundt.</h3>
+				<ul class="text-text/50">
+					<li><a href="/">Hjem</a></li>
+					<li><a href="/events">Events</a></li>
+					<li><a href="/artists">Kunstnere</a></li>
+					<li><a href="/om-os">Om os</a></li>
+					<li><a href="/sponsorer">Sponsorer</a></li>
+				</ul>
+			</div>
+			<!-- CONTACT INFORMATION -->
+			<div class="flex flex-1 flex-col items-end">
+				<h3 class="mb-2 font-black">KONNEKT&reg;</h3>
+				<ul class="text-text/50 flex flex-col items-end">
+					<a href="tel:+4512345678">+45 12 34 56 78</a>
+					<a href="mailto:konnekt@mail.dk">konnekt@mail.dk</a>
+					<a href="https://google.maps.com/konnekt">Konnektvej 17B, Konnekticut</a>
+				</ul>
+			</div>
+		</div>
+		<div class="text-text/50 flex justify-center text-xs">
+			<span>&copy;&nbsp;{new Date().getFullYear()}&nbsp;Foreningen&nbsp;KONNEKT&reg;</span>
+		</div>
+	</footer>
+{/snippet}
