462460c641914f8577745832e6dc8be3da02c039
Mattis Kristensen <mattismoel@gmail.com>
2025-02-18 19:53:44 +0100

    add user model to locals

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,13 +1,18 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
+
+import type { User } from "$lib/user";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: User | null
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
 	}
 }
 
-export {};
+export { };
