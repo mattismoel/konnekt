e09866091ad2050382f6f736b81abffcebba6d52
Mattis Kristensen <mattismoel@gmail.com>
2025-02-21 15:52:14 +0100

    add base dashboard events page

---
+++ frontend/src/routes/admin/dashboard/events/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import PlusIcon from '~icons/mdi/plus';
+	import EventEntry from './EventEntry.svelte';
+
+	let { data } = $props();
+
+	let { upcomingEvents } = $derived(data);
+</script>
+
+<div class="rounded-sm border border-zinc-800 bg-zinc-900 p-8">
+	<div class="flex justify-between">
+		<h1 class="mb-4 text-2xl font-bold">Kommende events.</h1>
+		<a href="/admin/events/edit" class="flex gap-2">
+			<PlusIcon class="text-xl" />
+			Tilføj event
+		</a>
+	</div>
+	<div class="divide-y">
+		{#each upcomingEvents as event (event.id)}
+			<EventEntry {event} />
+		{/each}
+	</div>
+</div>
---
+++ frontend/src/routes/admin/dashboard/events/EventEntry.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { earliestConcert, latestConcert } from '$lib/concert';
+	import type { Event } from '$lib/event';
+	import { formatDateStr } from '$lib/time';
+	import { format } from 'date-fns';
+	import TrashIcon from '~icons/mdi/trash';
+
+	type Props = {
+		event: Event;
+	};
+
+	let { event }: Props = $props();
+
+	const fromDate = $derived(earliestConcert(event.concerts)?.from || new Date());
+	const toDate = $derived(latestConcert(event.concerts)?.to || new Date());
+</script>
+
+<a
+	class="flex items-center gap-4 rounded-md border border-transparent p-2 hover:border-zinc-700 hover:bg-zinc-800"
+	href="/admin/events/edit?id={event.id}"
+>
+	<span class="flex-1">{event.title}</span>
+	<span class="line-clamp-1 flex-1 text-zinc-500"
+		>{formatDateStr(fromDate)}, {format(fromDate, 'HH:mm')}</span
+	>
+	<span class="line-clamp-1 flex-1 text-zinc-500"
+		>{formatDateStr(toDate)}, {format(toDate, 'HH:mm')}</span
+	>
+	<button class="text-zinc-400 hover:text-red-500">
+		<TrashIcon />
+	</button>
+</a>
