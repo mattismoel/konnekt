368d39587198f9322c27109a821f18b5b364cb69
Mattis Kristensen <mattismoel@gmail.com>
2025-02-21 12:57:50 +0100

    add Content-Type header to JSON responses

--- backend/internal/server/json.go
+++ backend/internal/server/json.go
@@ -6,6 +6,7 @@ import (
 )
 
 func writeJSON(w http.ResponseWriter, status int, data any) error {
+	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(data)
 }
