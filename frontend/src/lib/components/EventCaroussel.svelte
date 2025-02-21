566e1332d0b5ef2776cee7859531394b4446ebca
Mattis Kristensen <mattismoel@gmail.com>
2025-02-21 15:51:26 +0100

    add event caroussel component

---
+++ frontend/src/lib/components/EventCaroussel.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Event } from '$lib/event';
+	import EventCard from './EventCard.svelte';
+
+	type Props = {
+		title?: string;
+		events: Event[];
+	};
+
+	let { events, title = 'Events.' }: Props = $props();
+</script>
+
+<h3 class="mb-4 text-2xl font-bold">{title}</h3>
+<div class="flex gap-2">
+	{#each events as event (event.id)}
+		<EventCard {event} />
+	{/each}
+</div>
