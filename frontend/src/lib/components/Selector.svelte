daacc26856de7322d64d5d9ed54719a7002c63b4
Mattis Kristensen <mattismoel@gmail.com>
2025-02-24 20:44:43 +0100

    add selector component

---
+++ frontend/src/lib/components/Selector.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { cn } from '$lib/clsx';
+	import type { HTMLSelectAttributes } from 'svelte/elements';
+
+	type Entry = {
+		name: string;
+		value: string;
+	};
+
+	type Props = HTMLSelectAttributes & {
+		entries: Entry[];
+	};
+
+	let { entries, children, ...rest }: Props = $props();
+</script>
+
+<select {...rest} class={cn('rounded-sm border border-zinc-900 bg-zinc-950', rest.class)}>
+	{#each entries as entry (entry.value)}
+		<option value={entry.value}>{entry.name}</option>
+	{/each}
+</select>
