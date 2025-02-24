58afb6292647d546901cf2bb01e19d868a1137b3
Mattis Kristensen <mattismoel@gmail.com>
2025-02-24 20:44:16 +0100

    add datetime picker component

---
+++ frontend/src/lib/components/DateTimePicker.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import type { HTMLInputAttributes } from 'svelte/elements';
+	import Input from './Input.svelte';
+
+	type Props = HTMLInputAttributes & {
+		label: string;
+		date: Date;
+	};
+
+	let { label, date = $bindable(), ...rest }: Props = $props();
+</script>
+
+<Input
+	expandX
+	nonEmpty
+	type="datetime-local"
+	onchange={(e) => (date = e.currentTarget.valueAsDate || new Date())}
+	{label}
+	{...rest}
+/>
