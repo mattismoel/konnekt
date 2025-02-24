7d80965337b15a23b7d3aa7f7582488dd4dece21
Mattis Kristensen <mattismoel@gmail.com>
2025-02-24 20:33:32 +0100

    add card component

---
+++ frontend/src/lib/components/Card.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { cn } from '$lib/clsx';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	let { children, ...rest }: HTMLAttributes<HTMLDivElement> = $props();
+</script>
+
+<div class={cn('rounded-md border border-zinc-900 bg-zinc-950 p-8', rest.class)}>
+	{#if children}
+		{@render children()}
+	{/if}
+</div>
