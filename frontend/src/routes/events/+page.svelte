9b113c00b4ef6c4d5f04613b67ca5bdbfd5939c5
Mattis Kristensen <mattismoel@gmail.com>
2025-02-24 20:45:07 +0100

    update events page layout

--- frontend/src/routes/events/+page.svelte
+++ frontend/src/routes/events/+page.svelte
@@ -8,7 +8,7 @@
 <main class="min-h-sub-nav pt-20">
 	<div class="px-auto">
 		<h1 class="mb-4 text-3xl font-bold">Events.</h1>
-		<div class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+		<div class="flex gap-4">
 			{#each events as event (event.id)}
 				<EventCard {event} />
 			{/each}
