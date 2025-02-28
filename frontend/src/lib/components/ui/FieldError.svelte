b4e3b7aec204779052be1767fb3eba26148e0292
Mattis Kristensen <mattismoel@gmail.com>
2025-02-28 15:04:37 +0100

    add field error component for form validation errors

---
+++ frontend/src/lib/components/ui/FieldError.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { cn } from '$lib/clsx';
+
+	type Props = {
+		errors?: string[];
+		maxCount?: number;
+	};
+
+	let { errors, maxCount = 5 }: Props = $props();
+</script>
+
+<ul
+	class={cn('list-inside text-sm text-red-500', {
+		'list-disc': errors && errors.length > 1
+	})}
+>
+	{#each errors?.slice(0, maxCount) || [] as error}
+		<li>{error}</li>
+	{/each}
+</ul>
