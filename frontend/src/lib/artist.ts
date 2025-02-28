7f44727f5a31aff586fe593895f718161b1f9dba
Mattis Kristensen <mattismoel@gmail.com>
2025-02-28 15:04:45 +0100

    update error messages for artist form

--- frontend/src/lib/artist.ts
+++ frontend/src/lib/artist.ts
@@ -13,11 +13,25 @@ export const artistSchema = z.object({
 export type Artist = z.infer<typeof artistSchema>
 
 export const artistFormSchema = z.object({
-	name: z.string().nonempty(),
-	description: z.string().nonempty(),
-	imageUrl: z.string().nonempty().url(),
-	genreIds: z.number().positive().array(),
-	socials: z.string().nonempty().url().array(),
+	name: z
+		.string()
+		.nonempty({ message: "Kustnernavn skal være defineret" }),
+	description: z
+		.string()
+		.nonempty({ message: "Kunstnerbeskreivelse skal være defineret" }),
+	imageUrl: z
+		.string()
+		.nonempty({ message: "Kuntnerbillede skal være defineret" })
+		.url({ message: "Kunstnerbillede skal være gyldigt" }),
+	genreIds: z.number()
+		.positive()
+		.array()
+		.min(1, { message: "Mindst én genre skal være valgt" }),
+	socials: z
+		.string()
+		.nonempty()
+		.url({ message: "URL skal være gyldigt" })
+		.array(),
 })
 
 export type ArtistForm = z.infer<typeof artistFormSchema>
