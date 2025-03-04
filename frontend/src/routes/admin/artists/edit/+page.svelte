768877ff886e5f62595979c210c4224339af95b5
Mattis Kristensen <mattismoel@gmail.com>
2025-03-04 16:04:41 +0100

    refactor

--- frontend/src/routes/admin/artists/edit/+page.svelte
+++ frontend/src/routes/admin/artists/edit/+page.svelte
@@ -3,20 +3,12 @@
 	import ArtistForm from './ArtistForm.svelte';
 	import { createArtist, updateArtist, type artistFormSchema } from '$lib/artist';
 	import { page } from '$app/state';
-	import CreateConcertCard from '../../events/edit/CreateConcertCard.svelte';
 
 	let { data } = $props();
 
 	const submit = async (form: z.infer<typeof artistFormSchema>) => {
 		const id = page.url.searchParams.get('id');
-		if (id) {
-			const artist = await updateArtist(parseInt(id), form);
-			console.log('updated', artist);
-			return;
-		}
-
-		const artist = await createArtist(form);
-		console.log('created', artist);
+		id ? await updateArtist(parseInt(id), form) : createArtist(form);
 	};
 </script>
 
