527a3ce508da8aca7a5435e3e4b2b36af7b61705
Mattis Kristensen <mattismoel@gmail.com>
2025-03-07 21:40:38 +0100

    refactor of EventForm

--- frontend/src/routes/admin/events/edit/EventForm.svelte
+++ frontend/src/routes/admin/events/edit/EventForm.svelte
@@ -75,43 +75,68 @@
 </script>
 
 <form class="space-y-8" onsubmit={submit}>
-	<h1 class="mb-4 text-2xl font-bold">Lav event.</h1>
-	<ImagePreview src={coverImageUrl || ''} onChange={(file) => (form.coverImage = file)} />
-	<FieldError errors={formError?.flatten().fieldErrors['coverImage']} />
-	<div class="flex gap-4 *:flex-1">
-		<div class="flex flex-col gap-2">
-			<Input expandX label="Titel" type="text" name="title" bind:value={form.title} />
-			<FieldError errors={formError?.flatten().fieldErrors['title']} />
-		</div>
-		<div class="flex flex-col">
-			<Selector
-				onChange={(value) => (form.venueId = parseInt(value))}
-				class="w-full"
-				selected={form.venueId.toString()}
-				entries={venues.map((v) => ({
-					name: `${v.name}, ${v.city}`,
-					value: v.id.toString()
-				}))}
-			/>
-			<FieldError errors={formError?.flatten().fieldErrors['venueId']} />
-		</div>
+	<h1 class="mb-8 text-2xl font-bold">Lav event.</h1>
+	<div>
+		<ImagePreview src={coverImageUrl || ''} onChange={(file) => (form.coverImage = file)} />
+		<FieldError errors={formError?.flatten().fieldErrors['coverImageUrl']} />
 	</div>
-
-	<h1 class="mb-4 text-2xl font-bold">Koncerter.</h1>
-	<FieldError errors={formError?.flatten().fieldErrors['concerts']} />
-	<div class="space-y-4">
-		{#each form.concerts || [] as concert, i (concert.id)}
-			<CreateConcertCard
-				bind:concert={form.concerts[i]}
-				{artists}
-				idx={i + 1}
-				onDelete={() => deleteConcert(concert.id)}
-			/>
-		{/each}
-	</div>
-	<Button onclick={addConcert} expandX variant="ghost">+ Tilføj koncert</Button>
+	{@render generalSection()}
+	{@render concertsSection()}
 	<div class="flex gap-4">
 		<Button expandX variant="secondary">Preview</Button>
 		<Button type="submit" expandX>Offentligør</Button>
 	</div>
 </form>
+
+{#snippet generalSection()}
+	<div>
+		<h1 class="mb-4 text-2xl font-bold">Generelt.</h1>
+		<FieldError errors={formError?.flatten().fieldErrors['coverImage']} />
+		<div class="space-y-8">
+			<div class="flex gap-4 *:flex-1">
+				<Input
+					errors={formError?.flatten().fieldErrors['title']}
+					label="Titel"
+					type="text"
+					name="title"
+					bind:value={form.title}
+				/>
+				<div class="flex flex-col">
+					<Selector
+						onChange={(value) => (form.venueId = parseInt(value))}
+						class="w-full"
+						selected={form.venueId.toString()}
+						entries={venues.map((v) => ({
+							name: `${v.name}, ${v.city}`,
+							value: v.id.toString()
+						}))}
+					/>
+					<FieldError errors={formError?.flatten().fieldErrors['venueId']} />
+				</div>
+			</div>
+			<Input
+				errors={formError?.flatten().fieldErrors['description']}
+				label="Beskrivelse"
+				bind:value={form.description}
+			/>
+		</div>
+	</div>
+{/snippet}
+
+{#snippet concertsSection()}
+	<div class="space-y-6">
+		<h1 class="text-2xl font-bold">Koncerter.</h1>
+		<div class="space-y-4">
+			{#each form.concerts || [] as concert, i (concert.id)}
+				<CreateConcertCard
+					bind:concert={form.concerts[i]}
+					{artists}
+					idx={i + 1}
+					onDelete={() => deleteConcert(concert.id)}
+				/>
+			{/each}
+		</div>
+		<Button onclick={addConcert} expandX variant="ghost">+ Tilføj koncert</Button>
+		<FieldError errors={formError?.flatten().fieldErrors['concerts']} />
+	</div>
+{/snippet}
