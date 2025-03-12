4124a8e6b8c8ee4128df19e9a44d2bcf320e0a7a
Mattis Kristensen <mattismoel@gmail.com>
2025-03-12 17:22:11 +0100

    return artist events on artist page

--- frontend/src/routes/artists/[id]/+page.server.ts
+++ frontend/src/routes/artists/[id]/+page.server.ts
@@ -1,8 +1,13 @@
 import { artistById } from "$lib/artist";
+import { artistEvents } from "$lib/event";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params }) => {
   const artist = await artistById(parseInt(params.id))
+  const eventsResult = await artistEvents(parseInt(params.id))
 
-  return { artist }
+  return {
+    artist,
+    events: eventsResult.records,
+  }
 }
