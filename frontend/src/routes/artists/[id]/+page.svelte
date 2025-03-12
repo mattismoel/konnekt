f2eaeb0a4f6dd80c106fee6f6ad039476daeb41b
Mattis Kristensen <mattismoel@gmail.com>
2025-03-12 17:22:37 +0100

    update layout for artist page

--- frontend/src/routes/artists/[id]/+page.svelte
+++ frontend/src/routes/artists/[id]/+page.svelte
@@ -1,55 +1,54 @@
 <script lang="ts">
+	import { cn } from '$lib/clsx';
+	import EventCard from '$lib/components/EventCard.svelte';
+	import EventCaroussel from '$lib/components/EventCaroussel.svelte';
+	import SpotifyPreview from '$lib/components/SpotifyPreview.svelte';
 	import Fader from '$lib/components/ui/Fader.svelte';
 	import { socialUrlToIcon } from '$lib/social';
 	import { trackIdFromUrl } from '$lib/spotify.js';
 
 	let { data } = $props();
-	let { artist } = $derived(data);
+	let { artist, events } = $derived(data);
 
 	let trackId = $derived(trackIdFromUrl(artist.previewUrl));
+
+	let contentScrollPosY = $state(0);
+	$inspect(contentScrollPosY);
 </script>
 
 <main>
-	<div class="grid min-h-svh grid-cols-2">
-		<div class="relative isolate h-full">
-			<h1 class="absolute top-20 left-24 z-10 w-screen text-9xl font-bold">{artist.name}</h1>
-			<img src={artist.imageUrl} alt="Cover af {artist.name}" class="h-full object-cover" />
-			<Fader direction="left" class="absolute w-64 from-zinc-950" />
+	<div class="grid min-h-svh grid-cols-1 grid-rows-[85svh_1fr]">
+		<div class="px-auto relative isolate flex flex-col justify-end bg-blue-500 py-16">
+			<img
+				src={artist.imageUrl}
+				alt="Cover af {artist.name}"
+				class="absolute top-0 left-0 h-full w-full object-cover"
+			/>
+			<Fader direction="right" class="absolute w-96 from-zinc-950" />
+			<Fader direction="up" class="absolute h-96 from-zinc-950" />
+			<div class="z-10">
+				<h1 style:word-spacing="100vw" class="text-7xl font-bold md:text-8xl lg:text-9xl">
+					{artist.name}
+				</h1>
+			</div>
 		</div>
-		<article class="prose prose-invert flex flex-col gap-8 pt-64 pb-16 pl-24">
-			<div class="flex-1">
+		<article class="px-auto space-y-8 bg-zinc-950 py-16">
+			<!-- ARTICLE CONTENT -->
+			<div class="prose prose-invert max-w-none">
 				{@html artist.description}
 			</div>
+
 			{#if trackId}
-				{@render spotifyPreview(trackId)}
+				<SpotifyPreview {trackId} />
 			{/if}
-			{@render socials(artist.socials)}
+
+			{#if events.length > 0}
+				<div class="flex gap-2">
+					{#each events as event (event.id)}
+						<EventCard {event} class="" />
+					{/each}
+				</div>
+			{/if}
 		</article>
 	</div>
 </main>
-
-{#snippet spotifyPreview(trackId: string)}
-	{@const src = `https://open.spotify.com/embed/track/${trackId}?utm_source=generator&theme=0`}
-	<iframe
-		title="Audio preview"
-		style:border-radius="12px"
-		{src}
-		width="100%"
-		height="152"
-		frameBorder="0"
-		allowfullscreen
-		allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-		loading="lazy"
-	></iframe>
-{/snippet}
-
-{#snippet socials(urls: string[])}
-	<div class="flex w-full justify-end gap-4 text-2xl">
-		{#each urls as url (url)}
-			{@const Icon = socialUrlToIcon(url)}
-			<a href={url}>
-				<Icon />
-			</a>
-		{/each}
-	</div>
-{/snippet}
