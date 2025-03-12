cebb0ce182ea721322b7932f3107394ddc374677
Mattis Kristensen <mattismoel@gmail.com>
2025-03-12 18:13:57 +0100

    add faders to event caroussel

--- frontend/src/lib/components/EventCaroussel.svelte
+++ frontend/src/lib/components/EventCaroussel.svelte
@@ -1,18 +1,54 @@
 <script lang="ts">
+	import { cn } from '$lib/clsx';
 	import type { Event } from '$lib/event';
 	import EventCard from './EventCard.svelte';
+	import Fader from './ui/Fader.svelte';
 
 	type Props = {
 		title?: string;
 		events: Event[];
 	};
 
-	let { events, title = 'Events.' }: Props = $props();
+	let { events }: Props = $props();
+
+	let scrollX = $state(0);
+	let inner: HTMLDivElement;
+	let outer: HTMLDivElement;
+
+	let isScrolledToRight = $state(false);
+
+	const updateScroll = (newScrollX: number) => {
+		const innerWidth = inner.scrollWidth;
+		const outerWidth = outer.getBoundingClientRect().width;
+
+		const diff = innerWidth - outerWidth;
+		scrollX = newScrollX;
+
+		isScrolledToRight = scrollX >= diff;
+	};
 </script>
 
-<h3 class="mb-4 text-2xl font-bold">{title}</h3>
-<div class="flex gap-2">
-	{#each events as event (event.id)}
-		<EventCard {event} />
-	{/each}
+<div class="relative isolate" bind:this={outer}>
+	<!--RIGHT FADER -->
+	<Fader
+		direction="left"
+		class={cn('absolute z-10 w-32 from-zinc-950 transition-colors duration-300', {
+			'from-transparent': isScrolledToRight
+		})}
+	/>
+	<Fader
+		direction="right"
+		class={cn('absolute z-10 w-32 from-zinc-950 transition-colors duration-300', {
+			'from-transparent': scrollX <= 0
+		})}
+	/>
+	<div
+		bind:this={inner}
+		class="flex w-full gap-4 overflow-x-scroll"
+		onscroll={(e) => updateScroll(e.currentTarget.scrollLeft)}
+	>
+		{#each events as event (event.id)}
+			<EventCard {event} />
+		{/each}
+	</div>
 </div>
