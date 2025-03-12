8671e54d7cdd46179f8ed1e3e2dac4b543544c85
Mattis Kristensen <mattismoel@gmail.com>
2025-03-12 17:20:35 +0100

    fix styling

--- frontend/src/lib/components/SpotifyPreview.svelte
+++ frontend/src/lib/components/SpotifyPreview.svelte
@@ -9,14 +9,16 @@
 	);
 </script>
 
-<iframe
-	{src}
-	title="Audio preview"
-	style:border-radius="12px"
-	width="100%"
-	height="152"
-	frameBorder="0"
-	allowfullscreen
-	allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-	loading="lazy"
-></iframe>
+<div>
+	<iframe
+		{src}
+		title="Audio preview"
+		style:border-radius="12px"
+		width="100%"
+		height="152"
+		frameBorder="0"
+		allowfullscreen
+		allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+		loading="lazy"
+	></iframe>
+</div>
