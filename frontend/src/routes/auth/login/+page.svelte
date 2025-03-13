3b6292b4388dc637772f14b359c8d964f90d2a5c
Mattis Møl Kristensen <mattismoel@gmail.com>
2025-03-13 21:35:18 +0100

    update layout of login page

--- frontend/src/routes/auth/login/+page.svelte
+++ frontend/src/routes/auth/login/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { PUBLIC_BACKEND_URL } from '$env/static/public';
+	import Button from '$lib/components/ui/Button.svelte';
+	import Card from '$lib/components/ui/Card.svelte';
+	import Input from '$lib/components/ui/Input.svelte';
 
 	let email = $state('');
 	let password = $state('');
@@ -20,10 +23,13 @@
 	};
 </script>
 
-<main class="min-h-sub-nav">
-	<form class="flex flex-col gap-2" onsubmit={handleLogin}>
-		<input type="email" bind:value={email} placeholder="Email" />
-		<input type="password" bind:value={password} placeholder="Adgangskode" />
-		<button>Login</button>
-	</form>
+<main class="flex h-svh items-center justify-center">
+	<Card>
+		<h1 class="mb-8 text-2xl font-bold">Login.</h1>
+		<form class="flex flex-col gap-2" onsubmit={handleLogin}>
+			<Input type="email" bind:value={email} label="Email" />
+			<Input type="password" bind:value={password} label="Adgangskode" />
+			<Button expandX type="submit">Login</Button>
+		</form>
+	</Card>
 </main>
