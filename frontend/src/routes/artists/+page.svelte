6832929e46ce9790c9e4c6a6a49c52c4f5ff90df
Mattis Kristensen <mattismoel@gmail.com>
2025-03-14 11:55:55 +0100

    update styling

--- frontend/src/routes/artists/+page.svelte
+++ frontend/src/routes/artists/+page.svelte
@@ -30,11 +30,11 @@
 	>
 		<div class="grid w-full grid-cols-3">
 			<a href="/artists/{artist.id}" class="col-span-2 grid grid-cols-2 py-3 pl-3">
-				<span class="font-bold">{artist.name}</span>
-				<span>{artist.genres.map((g) => g.name).join(', ')}</span>
+				<span class="line-clamp-1 font-bold">{artist.name}</span>
+				<span class="line-clamp-1">{artist.genres.map((g) => g.name).join(', ')}</span>
 			</a>
 			<div
-				class="text-text/75 flex items-center justify-end gap-2 text-lg group-hover:text-zinc-700"
+				class="text-text/75 flex items-center justify-end gap-2 pr-3 text-lg group-hover:text-zinc-700"
 			>
 				{#each artist.socials as social}
 					{@const Icon = socialUrlToIcon(social)}
