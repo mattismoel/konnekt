43f0dd474a568f7a8fc2189519d5cc824da5a880
Mattis Kristensen <mattismoel@gmail.com>
2025-03-16 10:45:59 +0100

    add next upcoming event to events page

--- frontend/src/routes/events/+page.svelte
+++ frontend/src/routes/events/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import Caroussel from '$lib/components/Caroussel.svelte';
 	import EventCard from '$lib/components/EventCard.svelte';
+	import EventDetails from './[id]/EventDetails.svelte';
 
 	let { data } = $props();
 	let { events } = $derived(data);
 </script>
 
-<main class="min-h-sub-nav pt-20">
+<main class="min-h-sub-nav">
+	{#if events.length > 0}
+		<EventDetails event={events[0]} prefix="Næste event:" />
+	{/if}
 	<div class="px-auto">
 		<h1 class="mb-4 text-3xl font-bold">Events.</h1>
 		<Caroussel>
