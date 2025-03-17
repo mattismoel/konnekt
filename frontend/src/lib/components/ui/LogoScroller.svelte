52412cb1e895d6ccfcc15f52bdacd4d475812929
Mattis Kristensen <mattismoel@gmail.com>
2025-03-17 11:05:16 +0100

    add LogoScroller component
    >

---
+++ frontend/src/lib/components/ui/LogoScroller.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Fader from './Fader.svelte';
+
+	type Props = {
+		srcs: Map<string, string>;
+	};
+
+	let { srcs }: Props = $props();
+</script>
+
+<div class="relative isolate w-full overflow-hidden whitespace-nowrap">
+	<Fader direction="right" class="absolute z-50 w-32 from-zinc-900" />
+	<Fader direction="left" class="absolute z-50 w-32 from-zinc-900" />
+	{@render track(srcs)}
+	{@render track(srcs)}
+</div>
+
+{#snippet track(srcs: Map<string, string>)}
+	<div class="animate-slide inline-block w-fit flex-nowrap">
+		{#each [...srcs] as [name, src] (src)}
+			<img {src} alt={name} class="mx-8 inline h-16" />
+		{/each}
+	</div>
+{/snippet}
