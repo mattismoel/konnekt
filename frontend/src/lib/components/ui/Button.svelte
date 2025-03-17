6e687f70c04d971d50fa1bccf0f1f26cf9cfcea9
Mattis Kristensen <mattismoel@gmail.com>
2025-03-17 21:35:47 +0100

    remove redundant button props

--- frontend/src/lib/components/ui/Button.svelte
+++ frontend/src/lib/components/ui/Button.svelte
@@ -3,28 +3,17 @@
 	import type { HTMLButtonAttributes } from 'svelte/elements';
 
 	type Props = HTMLButtonAttributes & {
-		expandX?: boolean;
-		expandY?: boolean;
 		variant?: 'primary' | 'secondary' | 'dangerous' | 'ghost';
 	};
 
-	let {
-		children,
-		class: className,
-		expandX,
-		expandY = false,
-		variant = 'primary',
-		...rest
-	}: Props = $props();
+	let { children, class: className, variant = 'primary', ...rest }: Props = $props();
 </script>
 
 <button
 	type="button"
 	class={cn(
-		`flex h-min max-w-64 items-center justify-center gap-1 rounded-sm px-4 py-2 font-medium text-zinc-950`,
+		`flex h-min w-fit items-center justify-center gap-1 rounded-sm px-6 py-2 font-medium text-zinc-950`,
 		{
-			'w-full max-w-none': expandX,
-			'h-full': expandY,
 			'bg-zinc-100 text-zinc-950 hover:bg-zinc-300': variant === 'primary',
 			'border border-zinc-100 text-zinc-100 transition-colors hover:bg-zinc-300 hover:text-zinc-950':
 				variant === 'secondary',
