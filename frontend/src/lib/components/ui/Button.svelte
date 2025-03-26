a2f091ddd44f5b3e0e7478e09fd5bec8473cc258
Mattis Kristensen <mattismoel@gmail.com>
2025-03-26 21:13:16 +0100

    change dangerous button styling

--- frontend/src/lib/components/ui/Button.svelte
+++ frontend/src/lib/components/ui/Button.svelte
@@ -17,7 +17,8 @@
 			'bg-zinc-100 text-zinc-950 hover:bg-zinc-300': variant === 'primary',
 			'border border-zinc-100 text-zinc-100 transition-colors hover:bg-zinc-300 hover:text-zinc-950':
 				variant === 'secondary',
-			'border border-red-700 bg-red-800 text-red-300 hover:text-red-100': variant === 'dangerous',
+			'border border-red-900 bg-red-950 text-red-400 hover:border-red-800 hover:bg-red-900 hover:text-red-300':
+				variant === 'dangerous',
 			'border border-zinc-900 font-normal text-zinc-500 hover:bg-zinc-900': variant === 'ghost'
 		},
 		className
