2d22cb4eb7250912bc12bdcaeb4cd83a878e985b
Mattis Kristensen <mattismoel@gmail.com>
2025-03-26 21:17:16 +0100

    add venue util functions

--- frontend/src/lib/venue.ts
+++ frontend/src/lib/venue.ts
@@ -3,6 +3,12 @@ import { createListResult, type ListResult } from "./list-result";
 import { PUBLIC_BACKEND_URL } from "$env/static/public";
 import { APIError, apiErrorSchema } from "./error";
 
+export const venueForm = z.object({
+	name: z.string().nonempty(),
+	city: z.string().nonempty(),
+	countryCode: z.string().nonempty(),
+})
+
 export const venueSchema = z.object({
 	id: z.number().positive(),
 	name: z.string(),
@@ -26,4 +32,33 @@ export const listVenues = async (init?: RequestInit, params?: URLSearchParams): 
 	const result = createListResult(venueSchema).parse(await res.json())
 
 	return result
+}
+
+export const createVenue = async (form: z.infer<typeof venueForm>, init?: RequestInit) => {
+	const data = venueForm.parse(form)
+
+	const res = await fetch(`${PUBLIC_BACKEND_URL}/venues`, {
+		method: "POST",
+		credentials: "include",
+		body: JSON.stringify(data),
+		...init
+	})
+
+	if (!res.ok) {
+		const err = apiErrorSchema.parse(await res.json())
+		throw new APIError(res.status, "Could not create venue", err.message)
+	}
+}
+
+export const deleteVenue = async (id: number, init?: RequestInit) => {
+	const res = await fetch(`${PUBLIC_BACKEND_URL}/venues/${id}`, {
+		method: "DELETE",
+		credentials: "include",
+		...init,
+	})
+
+	if (!res.ok) {
+		const err = apiErrorSchema.parse(await res.json())
+		throw new APIError(res.status, "Could not create venue", err.message)
+	}
 }
