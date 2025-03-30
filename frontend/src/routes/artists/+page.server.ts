0f1e000455c668595789c09921dd2bf8e442bfb7
Mattis Kristensen <mattismoel@gmail.com>
2025-03-30 13:01:25 +0200

    implement upcoming artist display on artists page

--- frontend/src/routes/artists/+page.server.ts
+++ frontend/src/routes/artists/+page.server.ts
@@ -1,10 +1,23 @@
 import type { PageServerLoad } from "./$types";
+import { listUpcomingEvents } from "$lib/event";
 import { listArtists } from "$lib/artist";
+import { removeDuplicates } from "$lib/array";
 
 export const load: PageServerLoad = async ({ request }) => {
-  const artistsResult = await listArtists(new URLSearchParams())
+  let currentArtists = await listUpcomingEvents()
+    .then(({ records }) => records
+      .flatMap(({ concerts }) => concerts)
+      .map(({ artist }) => artist)
+    )
+
+  currentArtists = removeDuplicates(currentArtists)
+
+  console.log(currentArtists.map(a => ({ id: a.id, name: a.name })))
+
+  const { records: artists } = await listArtists(new URLSearchParams())
 
   return {
-    artists: artistsResult.records
+    currentArtists,
+    artists,
   }
 }
