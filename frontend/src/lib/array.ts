7c38cd4b8591335a0f00b93c13f2a149587c9f81
Mattis Kristensen <mattismoel@gmail.com>
2025-04-01 14:17:46 +0200

    add function for getting random element from generic array

--- frontend/src/lib/array.ts
+++ frontend/src/lib/array.ts
@@ -1,3 +1,23 @@
 export const removeDuplicates = <T extends { id: any }>(arr: T[]): T[] => {
 	return arr.filter((value, index, self) => index === self.findIndex((t) => t.id === value.id))
 }
+
+/**
+ * @description Returns a random value from the input array.
+ * If a selected object is input, the returned element is ensured not to be the same as the selected.
+ */
+export const pickRandom = <T extends { id: any }>(arr: T[], selected?: T): T | undefined => {
+	if (arr.length <= 0) return undefined
+
+	if (!selected) {
+		return arr[Math.floor(Math.random() * arr.length)]
+	}
+
+	let newEntry = selected
+
+	while (newEntry.id === selected.id) {
+		newEntry = arr[Math.floor(Math.random() * arr.length)]
+	}
+
+	return newEntry
+}
