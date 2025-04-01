de6a4265e2be84305fc266cd8ae4e7b67c87d68a
Mattis Kristensen <mattismoel@gmail.com>
2025-04-01 14:27:27 +0200

    use listUpcomingEvents util function

--- frontend/src/routes/events/+page.server.ts
+++ frontend/src/routes/events/+page.server.ts
@@ -1,17 +1,11 @@
 import type { PageServerLoad } from "./$types"
-import { startOfToday } from "date-fns"
-import { listEvents } from "$lib/event"
+import { listUpcomingEvents } from "$lib/event"
 
 export const load: PageServerLoad = async () => {
-  const result = await listEvents(new URLSearchParams({
-    "filter": [
-      "from_date" + ">=" + startOfToday().toISOString(),
-    ].join(","),
-    "order_by": "from_date desc"
-  }))
-
+  const { records } = await listUpcomingEvents()
+  console.log(records)
   return {
-    events: result.records
+    events: records
   }
 }
 
