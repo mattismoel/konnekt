6e2a9c7457e97b225daf93d3aa285bad68b8656e
Mattis Kristensen <mattismoel@gmail.com>
2025-04-01 14:23:52 +0200

    update events dashboard page

--- frontend/src/routes/admin/dashboard/events/+page.svelte
+++ frontend/src/routes/admin/dashboard/events/+page.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import Card from '$lib/components/ui/Card.svelte';
+	import Table from '$lib/components/ui/table/Table.svelte';
+	import TableBody from '$lib/components/ui/table/TableBody.svelte';
+	import TableCell from '$lib/components/ui/table/TableCell.svelte';
+	import TableHead from '$lib/components/ui/table/TableHead.svelte';
+	import TableHeader from '$lib/components/ui/table/TableHeader.svelte';
+	import TableRow from '$lib/components/ui/table/TableRow.svelte';
+	import { earliestConcert, latestConcert } from '$lib/concert';
+	import { format } from 'date-fns';
 	import EventEntry from './EventEntry.svelte';
 	import PlusIcon from '~icons/mdi/plus';
+	import { DATE_FORMAT, DATETIME_FORMAT } from '$lib/time';
+	import Button from '$lib/components/ui/Button.svelte';
+
+	import TrashIcon from '~icons/mdi/trash';
+	import EditIcon from '~icons/mdi/edit';
+	import { goto } from '$app/navigation';
 
 	let { data } = $props();
 
@@ -16,9 +30,44 @@
 			Tilføj event
 		</a>
 	</div>
-	<div class="divide-y">
-		{#each upcomingEvents as event (event.id)}
-			<EventEntry {event} />
-		{/each}
-	</div>
-</Card>
+	<section>
+		<Table>
+			<TableHead>
+				<TableRow>
+					<TableHeader>Eventtitel</TableHeader>
+					<TableHeader>Dato</TableHeader>
+					<TableHeader>Tidspunkt</TableHeader>
+					<TableHeader></TableHeader>
+				</TableRow>
+			</TableHead>
+			<TableBody class="divide-zinc-800">
+				{#each upcomingEvents as event (event.id)}
+					{@const fromDate = earliestConcert(event.concerts)?.from || new Date()}
+					{@const toDate = latestConcert(event.concerts)?.to || new Date()}
+
+					<TableRow class="hover:bg-zinc-800">
+						<TableCell class="font-medium">{event.title}</TableCell>
+						<TableCell class="text-text/50">{format(fromDate, DATE_FORMAT)}</TableCell>
+						<TableCell class="text-text/50"
+							>{format(fromDate, 'HH:mm')}-{format(toDate, 'HH:mm')}</TableCell
+						>
+						<TableCell>
+							<div class="flex justify-end gap-6">
+								<button
+									title="Redigér event"
+									onclick={() => goto(`/admin/events/edit?id=${event.id}`)}
+									class="text-text/50 hover:text-text"
+								>
+									<EditIcon />
+								</button>
+								<Button variant="dangerous">
+									<TrashIcon />
+								</Button>
+							</div>
+						</TableCell>
+					</TableRow>
+				{/each}
+			</TableBody>
+		</Table>
+	</section></Card
+>
