47c9c46d17cd283954c22ef24a2d398b2c350e0e
Mattis Kristensen <mattismoel@gmail.com>
2025-04-03 16:41:57 +0200

    add role guard to admin routes

--- frontend/src/routes/admin/+layout.server.ts
+++ frontend/src/routes/admin/+layout.server.ts
@@ -1,21 +1,12 @@
 import { redirect } from "@sveltejs/kit";
 import { PUBLIC_BACKEND_URL } from "$env/static/public";
 import type { LayoutServerLoad } from "./$types";
-import { roleSchema } from "$lib/auth";
+import { hasAllRoles, roleSchema } from "$lib/auth";
 
 export const load: LayoutServerLoad = async ({ locals, request }) => {
-  let res = await fetch(`${PUBLIC_BACKEND_URL}/users/roles/${locals.user?.id}`, {
-    credentials: "include",
+  if (!locals.user) return redirect(302, "/auth/login")
+
+  if (!await hasAllRoles(locals.roles, ["member"], {
     headers: request.headers
-  })
-
-  if (!res.ok) {
-    return redirect(307, "/auth/login")
-  }
-
-  const roles = roleSchema.array().parse(await res.json())
-
-  if (!roles.some(role => role.name === "admin")) {
-    return redirect(307, "/auth/login")
-  }
+  })) { return redirect(302, "/auth/login") }
 }
