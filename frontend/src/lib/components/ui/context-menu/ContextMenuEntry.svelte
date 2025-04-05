ee7a440f0a5d1a282a3b8d85dad492c73438147e
Mattis Kristensen <mattismoel@gmail.com>
2025-04-06 00:29:33 +0200

    add context menu component

---
+++ frontend/src/lib/components/ui/context-menu/ContextMenuEntry.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	type Props = HTMLAttributes<HTMLLIElement> & {
+		action: () => void;
+	};
+
+	let { action, children }: Props = $props();
+</script>
+
+<li class="text-text/75 hover:text-text py-2">
+	<button onclick={action}>
+		{@render children?.()}
+	</button>
+</li>
