5f299f067bb7be642b880ae388ae720df9d04e7e
Mattis Kristensen <mattismoel@gmail.com>
2025-04-06 13:43:12 +0200

    add disabled state to context menu entry

--- frontend/src/lib/components/ui/context-menu/ContextMenuEntry.svelte
+++ frontend/src/lib/components/ui/context-menu/ContextMenuEntry.svelte
@@ -3,13 +3,14 @@
 
 	type Props = HTMLAttributes<HTMLLIElement> & {
 		action: () => void;
+		disabled: boolean;
 	};
 
-	let { action, children }: Props = $props();
+	let { action, disabled, children }: Props = $props();
 </script>
 
-<li class="text-text/75 hover:text-text py-2">
-	<button onclick={action}>
+<li class="text-text/85 hover:text-text py-2">
+	<button {disabled} onclick={action} class="disabled:text-text/50">
 		{@render children?.()}
 	</button>
 </li>
