23abf8f898b00817d14659ef7ac8747def215ebe
Mattis Kristensen <mattismoel@gmail.com>
2025-04-13 10:28:40 +0200

    refactor event edit server loading

--- frontend/src/routes/admin/events/edit/+page.server.ts
+++ frontend/src/routes/admin/events/edit/+page.server.ts
@@ -1,44 +1,28 @@
 import type { PageServerLoad } from "./$types";
-import { error, redirect } from "@sveltejs/kit";
-import { APIError } from "$lib/error";
+import { redirect } from "@sveltejs/kit";
 import { eventById } from "$lib/event";
 import { listArtists } from "$lib/artist";
+import { hasPermissions, userPermissions } from "$lib/auth";
 import { listVenues } from "$lib/venue";
-import { hasPermissions } from "$lib/auth";
+import { userSession } from "$lib/user";
 
-export const load: PageServerLoad = async ({ locals, url, request }) => {
-  if (!hasPermissions(locals.permissions, ["view:event", "edit:event"])) {
+export const load: PageServerLoad = async ({ url, fetch }) => {
+  const user = await userSession(fetch)
+  const permissions = await userPermissions(fetch, user.id)
+
+  if (!hasPermissions(permissions, ["view:event", "edit:event"])) {
     return redirect(302, "/auth/login")
   }
 
-  try {
-    const artistsResult = await listArtists()
-    const venuesResult = await listVenues({
-      credentials: "include",
-      headers: request.headers,
-    })
+  const { records: artists } = await listArtists(fetch)
+  const { records: venues } = await listVenues(fetch)
 
-    const id = url.searchParams.get("id")
-    if (!id) {
-      return {
-        event: null,
-        venues: venuesResult.records,
-        artists: artistsResult.records,
-      }
-    }
-
-    const event = await eventById(parseInt(id))
-
-    return {
-      event,
-      venues: venuesResult.records,
-      artists: artistsResult.records,
-    }
-  } catch (e) {
-    if (e instanceof APIError) {
-      return error(e.status, e.message)
-    }
-
-    return error(500, "Could not load")
+  const id = url.searchParams.get("id")
+  if (!id) {
+    return { event: null, venues, artists }
   }
+
+  const event = await eventById(fetch, parseInt(id))
+
+  return { event, venues, artists }
 }
