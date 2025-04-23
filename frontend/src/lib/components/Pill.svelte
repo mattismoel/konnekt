d4bb9dafc7dc2bc25f287d3d7e16ccc6e51a8145
Mattis Kristensen <mattismoel@gmail.com>
2025-04-23 14:57:17 +0200

    implement pill sizing

--- frontend/src/lib/components/Pill.svelte
+++ frontend/src/lib/components/Pill.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
-	let { children } = $props();
+	import { cn } from '$lib/clsx';
+	import type { HTMLLiAttributes } from 'svelte/elements';
+
+	type Size = 'sm' | 'base';
+
+	type Props = HTMLLiAttributes & {
+		size?: Size;
+	};
+
+	let { size = 'sm', children, ...rest }: Props = $props();
 </script>
 
-<div class="rounded-full border border-zinc-800 bg-zinc-900 px-3 py-2">
-	{@render children()}
-</div>
+<li
+	{...rest}
+	class={cn('h-min w-fit rounded-full border border-zinc-800 bg-zinc-900 px-3 py-2', {
+		'text-sm': size === 'sm'
+	})}
+>
+	{@render children?.()}
+</li>
