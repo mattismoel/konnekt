599b22dd18665d0d9baa30ee8f9b7d999a8e2056
Mattis Kristensen <mattismoel@gmail.com>
2025-04-29 13:40:09 +0200

    refactor events by id page

--- frontend/src/routes/(app)/events/[id]/+page.svelte
+++ frontend/src/routes/(app)/events/[id]/+page.svelte
@@ -5,25 +5,24 @@
 	import EventDetails from './EventDetails.svelte';
 
 	const { data } = $props();
-	const { event, recommendedEvents } = $derived(data);
-
-	const eventArtists = $derived(event.concerts.map((c) => c.artist));
 </script>
 
 <main class="min-h-sub-nav flex flex-col gap-16 pb-16 text-white">
-	<EventDetails active {event} />
+	<EventDetails active event={data.event} />
 	<article class="px-auto space-y-16 pt-8 pb-16">
 		<section class="prose prose-invert max-w-none">
-			{@html event.description}
+			{@html data.event.description}
 		</section>
-		<EventCalendar {event} />
-		<section>
-			<h1 class="mb-4 text-2xl font-bold">Se også</h1>
-			<Caroussel>
-				{#each recommendedEvents as event (event.id)}
-					<EventCard {event} />
-				{/each}
-			</Caroussel>
-		</section>
+		<EventCalendar event={data.event} />
+		{#if data.recommendedEvents.length > 0}
+			<section>
+				<h1 class="mb-4 text-2xl font-bold">Se også</h1>
+				<Caroussel>
+					{#each data.recommendedEvents as event (event.id)}
+						<EventCard {event} />
+					{/each}
+				</Caroussel>
+			</section>
+		{/if}
 	</article>
 </main>
