a616c8267e7a1948778d4ab96e61dc97f9f5520c
Mattis Møl Kristensen <mattismoel@gmail.com>
2025-04-30 15:21:11 +0200

    return venue on creation

--- backend/internal/server/venue.go
+++ backend/internal/server/venue.go
@@ -50,13 +50,24 @@ func (s Server) handleCreateVenue() http.HandlerFunc {
 			return
 		}
 
-		_, err = s.venueService.Create(ctx, service.CreateVenue{
+		venueID, err := s.venueService.Create(ctx, service.CreateVenue{
 			Name:        load.Name,
 			CountryCode: load.CountryCode,
 			City:        load.City,
 		})
 
 		if err != nil {
+			writeError(w, err)
+			return
+		}
+
+		venue, err := s.venueService.ByID(ctx, venueID)
+		if err != nil {
+			writeError(w, err)
+			return
+		}
+
+		if err := writeJSON(w, http.StatusCreated, venue); err != nil {
 			writeError(w, err)
 			return
 		}
