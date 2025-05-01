3150157f29867277a4995776ab0e49352e1e8019
Mattis Kristensen <mattismoel@gmail.com>
2025-05-01 17:58:35 +0200

    clean up venue entry

--- frontend/src/routes/admin/venues/VenueEntry.svelte
+++ frontend/src/routes/admin/venues/VenueEntry.svelte
@@ -1,13 +1,6 @@
 <script lang="ts">
-	import { cn } from '$lib/clsx';
-	import Selector from '$lib/components/ui/Selector.svelte';
-	import { COUNTRIES_MAP } from '$lib/features/venue/countries';
-	import { deleteVenue, editVenue, type Venue, type venueForm } from '$lib/features/venue/venue';
-	import type { z } from 'zod';
+	import { deleteVenue, type Venue } from '$lib/features/venue/venue';
 
-	import CheckIcon from '~icons/mdi/check';
-	import MenuIcon from '~icons/mdi/dots-vertical';
-	import XIcon from '~icons/mdi/close';
 	import ContextMenu from '$lib/components/ui/context-menu/ContextMenu.svelte';
 	import ContextMenuEntry from '$lib/components/ui/context-menu/ContextMenuEntry.svelte';
 	import { hasPermissions, type Permission } from '$lib/features/auth/permission';
@@ -24,39 +17,6 @@
 
 	let { venue, memberPermissions }: Props = $props();
 	let showContextMenu = $state(false);
-	let form = $state<z.infer<typeof venueForm>>({
-		...(venue || {
-			name: '',
-			city: '',
-			countryCode: ''
-		})
-	});
-
-	let isEdited = $derived(
-		form.name !== venue?.name ||
-			form.city !== venue?.city ||
-			form.countryCode !== venue?.countryCode
-	);
-
-	const resetForm = () => {
-		form = { ...(venue || { name: '', city: '', countryCode: '' }) };
-	};
-
-	const handleEditVenue = async () => {
-		try {
-			await editVenue(fetch, venue.id, form);
-			toaster.addToast('Venue redigeret');
-			await invalidateAll();
-		} catch (e) {
-			if (e instanceof APIError) {
-				toaster.addToast('Kunne ikke regigere venue', e.cause, 'error');
-				return;
-			}
-
-			toaster.addToast('Kunne ikke redigere venue', 'Noget gik galt...', 'error');
-			return;
-		}
-	};
 
 	const handleDeleteVenue = async () => {
 		if (!confirm(`Er sikker på, at du vil slette venue "${venue.name}"?`)) return;
@@ -76,12 +36,6 @@
 		}
 	};
 </script>
-
-<svelte:window
-	onkeydown={(e) => {
-		if (e.key === 'Escape' && isEdited) resetForm();
-	}}
-/>
 
 <ListEntry class="">
 	<a href="/admin/venues/edit/{venue.id}" class="w-full">
