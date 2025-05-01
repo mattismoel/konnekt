124a6c73882f8813f62af90b960c33d371d5ed02
Mattis Kristensen <mattismoel@gmail.com>
2025-05-01 14:40:13 +0200

    change heading of upcoming artist events

--- frontend/src/routes/(app)/artists/[id]/+page.svelte
+++ frontend/src/routes/(app)/artists/[id]/+page.svelte
@@ -57,7 +57,7 @@
 
 			{#if events.length > 0}
 				<section>
-					<h1 class="font-heading mb-8 text-2xl font-bold">Kommende events.</h1>
+					<h1 class="font-heading mb-8 text-2xl font-bold">Oplev {artist.name} her</h1>
 					<Caroussel>
 						{#each events as event (event.id)}
 							<EventCard {event} />
