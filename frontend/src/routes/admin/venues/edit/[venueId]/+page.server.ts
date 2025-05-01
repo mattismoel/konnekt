b3ad4ffc09748b1cd380ca34673d06a93a187e45
Mattis Kristensen <mattismoel@gmail.com>
2025-05-01 18:15:44 +0200

    fix param import type

--- frontend/src/routes/admin/venues/edit/[venueId]/+page.server.ts
+++ frontend/src/routes/admin/venues/edit/[venueId]/+page.server.ts
@@ -1,8 +1,8 @@
 import { venueById } from "$lib/features/venue/venue";
-import type { PageServerLoad } from "../$types";
+import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params, fetch }) => {
-	const venue = await venueById(fetch, params.venueId)
+	const venue = await venueById(fetch, parseInt(params.venueId))
 
 	return { venue }
 }
