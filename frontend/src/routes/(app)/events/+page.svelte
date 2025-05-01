b0dd90033e788285d4651b0eac5657bc5e0735c4
Mattis Kristensen <mattismoel@gmail.com>
2025-05-01 15:11:14 +0200

    add padding y to events page

--- frontend/src/routes/(app)/events/+page.svelte
+++ frontend/src/routes/(app)/events/+page.svelte
@@ -11,7 +11,7 @@
 	{#if events.length > 0}
 		<EventDetails event={events[0]} prefix="Næste event:" />
 	{/if}
-	<div class="px-auto">
+	<div class="px-auto py-16">
 		<h1 class="mb-4 text-3xl font-bold">Events</h1>
 		<Caroussel>
 			{#each events as event (event.id)}
