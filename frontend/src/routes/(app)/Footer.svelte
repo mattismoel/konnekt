16b285f8c75fcada4a565ab8949467b55e6a63bd
Mattis Kristensen <mattismoel@gmail.com>
2025-05-02 19:06:14 +0200

    fix footer layout on smaller devices

--- frontend/src/routes/(app)/Footer.svelte
+++ frontend/src/routes/(app)/Footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <footer class="px-auto border-t border-t-zinc-900 bg-zinc-950 pt-8 pb-6">
-	<div class="mb-8 grid w-full grid-cols-1 sm:grid-cols-2">
+	<div class="mb-8 grid w-full grid-cols-1 gap-8 sm:grid-cols-2">
 		<!-- NAVIGATION -->
 		<div class="flex-1">
 			<h3 class="font-heading mb-2 font-bold">Find rundt.</h3>
@@ -15,10 +15,11 @@
 			</ul>
 		</div>
 		<!-- CONTACT INFORMATION -->
-		<div class="flex flex-1 flex-col items-end">
-			<Logo class="mb-2 h-4" />
+		<div class="flex flex-1 flex-col items-start sm:items-end">
+			<Logo class="mb-2 hidden h-4 sm:block" />
+			<h3 class="font-heading mb-2 font-bold">Kontakt os</h3>
 			<address class="not-italic">
-				<ul class="text-text/50 flex flex-col items-end">
+				<ul class="text-text/50 flex flex-col items-start sm:items-end">
 					<a href="tel:+4512345678">+45 12 34 56 78</a>
 					<a href="mailto:konnekt@mail.dk">konnekt@mail.dk</a>
 					<a href="https://google.maps.com/konnekt">Konnektvej 17B, Konnekticut</a>
