1c30847170306f97c3f005037481c7aaff0330d3
Mattis Kristensen <mattismoel@gmail.com>
2025-05-03 16:51:34 +0200

    add 'SearchList' component

---
+++ frontend/src/lib/components/SearchList.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { WithChildren } from '$lib/types';
+	import SearchBar from './SearchBar.svelte';
+	import List from './ui/List.svelte';
+
+	type Props = WithChildren<{
+		search: string;
+	}>;
+
+	let { search = $bindable(''), children }: Props = $props();
+</script>
+
+<div class="flex flex-col gap-8">
+	<SearchBar bind:value={search} />
+	<List>
+		{@render children?.()}
+	</List>
+</div>
