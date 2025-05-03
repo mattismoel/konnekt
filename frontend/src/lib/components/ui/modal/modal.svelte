8bdd2f5a63eda12b33aad631a6e0cde58456889c
Mattis Kristensen <mattismoel@gmail.com>
2025-05-03 15:48:17 +0200

    change default modal width

--- frontend/src/lib/components/ui/modal/modal.svelte
+++ frontend/src/lib/components/ui/modal/modal.svelte
@@ -20,7 +20,7 @@
 	onclick={(e) => e.target === dialog && dialog.close()}
 	bind:this={dialog}
 	class={cn(
-		'fixed top-1/2 left-1/2 min-w-96 -translate-x-1/2 -translate-y-1/2 flex-col overflow-hidden rounded-md border border-zinc-800',
+		'fixed top-1/2 left-1/2 w-full min-w-xs -translate-x-1/2 -translate-y-1/2 flex-col overflow-hidden rounded-md border border-zinc-800 sm:min-w-lg',
 		rest.class
 	)}
 >
