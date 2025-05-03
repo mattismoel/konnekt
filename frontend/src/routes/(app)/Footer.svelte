664406630310cc8a73195ca744bacec777156f64
Mattis Kristensen <mattismoel@gmail.com>
2025-05-03 12:40:51 +0200

    small alignment changes

--- frontend/src/routes/(app)/Footer.svelte
+++ frontend/src/routes/(app)/Footer.svelte
@@ -6,7 +6,7 @@
 	<div class="mb-8 grid w-full grid-cols-1 gap-8 sm:grid-cols-2">
 		<!-- NAVIGATION -->
 		<div class="flex-1">
-			<h3 class="font-heading mb-2 font-bold">Find rundt</h3>
+			<h3 class="font-heading mb-2 inline-block align-top leading-none font-bold">Find rundt</h3>
 			<ul class="text-text/50">
 				<li><a href="/">Hjem</a></li>
 				<li><a href="/events">Events</a></li>
@@ -17,7 +17,7 @@
 		<!-- CONTACT INFORMATION -->
 		<div class="flex flex-1 flex-col items-start sm:items-end">
 			<Logo class="mb-2 hidden h-4 sm:block" />
-			<h3 class="font-heading mb-2 font-bold">Kontakt os</h3>
+			<h3 class="font-heading mb-2 font-bold sm:hidden">Kontakt os</h3>
 			<address class="not-italic">
 				<ul class="text-text/50 flex flex-col items-start sm:items-end">
 					<a href="tel:+4512345678">+45 12 34 56 78</a>
