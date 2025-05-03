b59cb2d60e8f001397db1134b6e3b99fbfe996bd
Mattis Kristensen <mattismoel@gmail.com>
2025-05-03 16:51:46 +0200

    refactor admin artist list to use 'SearchList' component

--- frontend/src/routes/admin/artists/ArtistList.svelte
+++ frontend/src/routes/admin/artists/ArtistList.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import SearchBar from '$lib/components/SearchBar.svelte';
-	import List from '$lib/components/ui/List.svelte';
+	import SearchList from '$lib/components/SearchList.svelte';
 	import { type Artist } from '$lib/features/artist/artist';
 	import type { Permission } from '$lib/features/auth/permission';
 	import ArtistEntry from './ArtistEntry.svelte';
@@ -20,28 +19,21 @@
 	);
 </script>
 
-<div class="flex flex-col gap-8">
-	<SearchBar bind:value={search} />
+<SearchList bind:search>
 	{#if search}
-		<List>
-			{#each filteredArtists as artist (artist.id)}
-				<ArtistEntry {artist} {memberPermissions} />
-			{/each}
-		</List>
+		{#each filteredArtists as artist (artist.id)}
+			<ArtistEntry {artist} {memberPermissions} />
+		{/each}
 	{:else}
-		<List>
-			{#each upcomingArtists as artist (artist.id)}
-				<ArtistEntry {artist} {memberPermissions} />
-			{/each}
-		</List>
+		{#each upcomingArtists as artist (artist.id)}
+			<ArtistEntry {artist} {memberPermissions} />
+		{/each}
 
 		<details>
 			<summary class="mb-4">Alle kunstnere</summary>
-			<List>
-				{#each artists as artist (artist.id)}
-					<ArtistEntry {artist} {memberPermissions} />
-				{/each}
-			</List>
+			{#each artists as artist (artist.id)}
+				<ArtistEntry {artist} {memberPermissions} />
+			{/each}
 		</details>
 	{/if}
-</div>
+</SearchList>
