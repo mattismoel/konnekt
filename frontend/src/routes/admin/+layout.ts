ff120c1a0aceb084bfae6f0e948c1a41c800fe98
Mattis Kristensen <mattismoel@gmail.com>
2025-05-04 17:26:31 +0200

    redirect when no session

--- frontend/src/routes/admin/+layout.ts
+++ frontend/src/routes/admin/+layout.ts
@@ -1,23 +1,23 @@
 import type { LayoutLoad } from "./$types";
-import { redirect } from "@sveltejs/kit";
 
 import { hasAllTeams } from "$lib/features/auth/team";
 import { memberSession } from "$lib/features/auth/member";
 import { authStore } from "$lib/auth.svelte";
-
+import { redirect } from "@sveltejs/kit";
 
 export const load: LayoutLoad = async ({ fetch }) => {
-  const member = await memberSession(fetch)
+  try {
+    const member = await memberSession(fetch)
+    if (!hasAllTeams(member.teams, ["member"])) {
+      return redirect(302, "/auth/login")
+    }
 
-  if (!hasAllTeams(member.teams, ["member"])) {
+    authStore.auth = {
+      member,
+      permissions: member.permissions,
+      teams: member.teams,
+    }
+  } catch (e) {
     return redirect(302, "/auth/login")
   }
-
-
-  authStore.auth = {
-    member,
-    permissions: member.permissions,
-    teams: member.teams,
-  }
-  console.log(authStore.permissions)
 }
