e82eec704a01e24c674965eff1c0afe4c4d310d3
Mattis Kristensen <mattismoel@gmail.com>
2025-05-05 19:45:26 +0200

    add menu button to navbar

--- frontend/src/routes/(app)/+layout.svelte
+++ frontend/src/routes/(app)/+layout.svelte
@@ -4,12 +4,16 @@
 	import Footer from './Footer.svelte';
 	import * as Navbar from '$lib/components/navbar/index';
 	import Logo from '$lib/assets/Logo.svelte';
+	import MenuIcon from '~icons/mdi/menu';
 
 	let { children } = $props();
 </script>
 
 <Navbar.Root>
 	<Navbar.Header>
+		<button>
+			<MenuIcon class="text-xl" onclick={() => (navMenuExpanded = true)} />
+		</button>
 		<a href="/">
 			<Logo class="h-4" />
 		</a>
