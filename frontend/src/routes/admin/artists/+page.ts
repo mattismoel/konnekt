d66fa33cdfbaca10ef940d6885c2f0b516fa56f2
Mattis Kristensen <mattismoel@gmail.com>
2025-05-07 15:48:56 +0200

    remove view permissions from artist pages
    >

--- frontend/src/routes/admin/artists/+page.ts
+++ frontend/src/routes/admin/artists/+page.ts
@@ -1,20 +1,11 @@
 import { eventsArtists, listArtists } from "$lib/features/artist/artist";
-import { redirect } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
-import { memberSession } from "$lib/features/auth/member";
-import { hasSomeTeam } from "$lib/features/auth/team";
 import { listUpcomingEvents } from "$lib/features/event/event";
 
 export const load: PageLoad = async ({ fetch }) => {
-  const member = await memberSession(fetch)
-
   const { records: upcomingEvents } = await listUpcomingEvents(fetch)
 
   const upcomingArtists = eventsArtists(upcomingEvents)
-
-  if (!hasSomeTeam(member.teams, ["admin", "event-management"])) {
-    return redirect(302, "/auth/login")
-  }
 
   const { records: artists } = await listArtists(fetch)
 
--- frontend/src/routes/admin/artists/edit/[artistId]/+page.ts
+++ frontend/src/routes/admin/artists/edit/[artistId]/+page.ts
@@ -1,18 +1,9 @@
 import type { PageLoad } from "./$types";
-import { redirect } from "@sveltejs/kit";
 
 import { artistById } from "$lib/features/artist/artist";
 import { listGenres } from "$lib/features/artist/genre";
-import { hasPermissions } from "$lib/features/auth/permission";
-import { memberSession } from "$lib/features/auth/member";
 
 export const load: PageLoad = async ({ params, fetch }) => {
-  const member = await memberSession(fetch)
-
-  if (!hasPermissions(member.permissions, ["view:artist", "edit:artist"])) {
-    return redirect(302, "/auth/login")
-  }
-
   const { records: genres } = await listGenres(fetch)
 
   const artist = await artistById(fetch, parseInt(params.artistId))
