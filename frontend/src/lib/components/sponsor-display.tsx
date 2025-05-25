c9e894dd4d5eb79d277258f232471e5f6e65ae6f
Mattis Kristensen <mattismoel@gmail.com>
2025-05-26 01:36:06 +0200

    update landing page

---
+++ frontend/src/lib/components/sponsor-display.tsx
@@ -0,0 +1,19 @@
+type Src = string
+
+type Props = {
+	srcs: Map<string, Src>
+}
+
+const SponsorDisplay = ({ srcs }: Props) => {
+	return (
+		<div className="p-8 sm:p-16 justify-center gap-12 flex flex-wrap bg-radial from-zinc-900 to-background to-[75%]">
+			{Array.from(srcs).map(([href, src]) => (
+				<a href={href} className="w-20 sm:w-24 group flex items-center justify-center">
+					<img src={src} className="aspect-square brightness-60 group-hover:brightness-100 transition-[filter,scale]" />
+				</a>
+			))}
+		</div>
+	)
+}
+
+export default SponsorDisplay
