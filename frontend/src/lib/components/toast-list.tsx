035dd3a1455c1f43cb57a1814e16f592e2fa330f
Mattis Kristensen <mattismoel@gmail.com>
2025-05-26 23:22:57 +0200

    refactor toasts

--- frontend/src/lib/components/toast-list.tsx
+++ frontend/src/lib/components/toast-list.tsx
@@ -1,14 +1,27 @@
-import type { IconType } from 'react-icons/lib';
-import { cn } from '../clsx';
-import type { Severity, Toast as ToastType } from '../context/toast';
-import { FaXmark } from "react-icons/fa6"
-import { FaExclamationCircle, FaExclamationTriangle, FaInfoCircle } from 'react-icons/fa';
+import type { IconType } from "react-icons/lib";
+import { type Severity, useToast } from "../context/toast"
+import { FaExclamationCircle, FaExclamationTriangle, FaInfoCircle } from "react-icons/fa";
+import { type Toast as ToastType } from "../context/toast";
+import { cn } from "../clsx";
+import { FaXmark } from "react-icons/fa6";
 
 let iconMap = new Map<Severity, IconType>([
 	['warning', FaExclamationTriangle],
 	['info', FaInfoCircle],
 	['error', FaExclamationCircle]
 ]);
+
+const ToastList = () => {
+	const { toasts, removeToast } = useToast()
+
+	return (
+		<div className='fixed bottom-8 right-8 z-50 flex flex-col gap-2'>
+			{toasts.map(toast => (
+				<Toast key={toast.id} toast={toast} onDelete={() => removeToast(toast.id)} />
+			))}
+		</div>
+	)
+}
 
 
 type Props = {
@@ -63,4 +76,4 @@ const Toast = ({ toast, onDelete }: Props) => {
 	)
 }
 
-export default Toast
+export default ToastList
