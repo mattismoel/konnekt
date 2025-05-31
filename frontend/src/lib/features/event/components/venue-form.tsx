d74ce562be1e7a4171228f79ee17d67dd0c4d36f
Mattis Kristensen <mattismoel@gmail.com>
2025-05-31 20:21:39 +0200

    add disabled state to venue form

--- frontend/src/lib/features/event/components/venue-form.tsx
+++ frontend/src/lib/features/event/components/venue-form.tsx
@@ -1,6 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { useQueryClient } from '@tanstack/react-query';
 
 import { COUNTRIES_MAP } from '../countries';
@@ -11,22 +11,25 @@ import Button from '@/lib/components/ui/button/button';
 import FormField from '@/lib/components/form-field';
 import Input from '@/lib/components/ui/input';
 import Selector from '@/lib/components/ui/selector';
+import { useAuth } from '@/lib/context/auth';
 
 type Props = {
 	venue?: Venue
-	disabled?: boolean;
 }
 
-const VenueForm = ({ venue, disabled = false }: Props) => {
+const VenueForm = ({ venue }: Props) => {
+	const { hasPermissions } = useAuth()
 	const queryClient = useQueryClient()
 
+	const isEditable = hasPermissions(["edit:venue"])
+
 	const {
+		control,
 		formState: { errors },
 		register,
 		handleSubmit,
-		getValues,
-		setValue,
 	} = useForm({
+		disabled: !isEditable,
 		defaultValues: { ...venue },
 		resolver: zodResolver(venueForm),
 	})
@@ -45,31 +48,36 @@ const VenueForm = ({ venue, disabled = false }: Props) => {
 		<form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-8">
 			<div className="flex flex-col gap-4">
 				<FormField error={errors.name}>
-					<Input {...register("name")} disabled={disabled} placeholder="Venuenavn" />
+					<Input {...register("name")} placeholder="Venuenavn" />
 				</FormField>
 
 				<div className="flex gap-4">
 					<FormField error={errors.city}>
-						<Input {...register("city")} disabled={disabled} placeholder="By" className="flex-1" />
+						<Input {...register("city")} placeholder="By" className="flex-1" />
 					</FormField>
 					<div>
-						<FormField error={errors.countryCode}>
-							<Selector
-								onChange={(e) => setValue("countryCode", e.target.value)}
-								disabled={disabled}
-								className="h-min w-min"
-								placeholder="Vælg land..."
-								defaultValue={getValues("countryCode")}
-							>
-								{Array.from(COUNTRIES_MAP).map(([value, name]) => (
-									<option value={value} key={value}>{name}</option>
-								))}
-							</Selector>
-						</FormField>
+						<Controller
+							control={control}
+							name="countryCode"
+							render={({ field: { onChange, ...rest } }) => (
+								<FormField error={errors.countryCode}>
+									<Selector
+										{...rest}
+										onChange={(e) => onChange(e.target.value)}
+										placeholder="Vælg land..."
+										className="h-min w-min"
+									>
+										{Array.from(COUNTRIES_MAP).map(([value, name]) => (
+											<option value={value} key={value}>{name}</option>
+										))}
+									</Selector>
+								</FormField>
+							)}
+						/>
 					</div>
 				</div>
 			</div>
-			{!disabled && <Button type="submit" className="w-full">Offentliggør</Button>}
+			{isEditable && <Button type="submit" className="w-full">Offentliggør</Button>}
 		</form>
 	)
 }
