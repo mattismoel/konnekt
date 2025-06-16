60b8799babeba41e44d524b3f96a47e2e71ddf9d
Mattis Kristensen <mattismoel@gmail.com>
2025-06-16 14:25:47 +0200

    formatting

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -2,9 +2,9 @@ import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { QueryClient } from "@tanstack/react-query"
 
 type RouteProps = {
-  queryClient: QueryClient
+	queryClient: QueryClient
 }
 
 export const Route = createRootRouteWithContext<RouteProps>()({
-  component: Outlet,
+	component: Outlet,
 })
