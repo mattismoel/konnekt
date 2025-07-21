4a4f232f9cad4b486c0899927180a9a8541a1502
Mattis Kristensen <mattismoel@gmail.com>
2025-07-21 19:11:52 +0200

    implement random image selection for slideshow

--- frontend/src/lib/components/slideshow.tsx
+++ frontend/src/lib/components/slideshow.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState, type ImgHTMLAttributes } from "react";
 import { cn } from "../clsx";
 
+// The amount of images to have passed, before the same image can be shown 
+// again.
+const HISTORY_SIZE = 3;
+
 // The amount of seconds each image is to be shown.
 const IMAGE_HOLD_SECS = 4.0
 
@@ -13,15 +17,24 @@ type Props = {
 }
 
 const Slideshow = ({ srcs }: Props) => {
-	const [currentIdx, setCurrentIdx] = useState(0)
-
+	let [indexHistory, setIndexHistory] = useState([0]);
+	const currentIdx = indexHistory[indexHistory.length - 1]
 
 	useEffect(() => {
 		if (srcs.length <= 0) return
 
 		const interval = setInterval(() => {
-			setCurrentIdx(prev => (prev + 1) % srcs.length)
-			// Logic here...
+			setIndexHistory(prevHistory => {
+				let nextIdx: number;
+
+				do {
+					nextIdx = Math.floor(Math.random() * srcs.length)
+				} while (prevHistory.includes(nextIdx))
+
+				const newHistory = [...prevHistory, nextIdx].slice(-HISTORY_SIZE);
+
+				return newHistory
+			})
 		}, rate * 1000)
 
 		return () => clearInterval(interval);
