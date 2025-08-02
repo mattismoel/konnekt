cb593a1a26310242bb215ca5cf78995f3a37dfc2
Mattis Kristensen <mattismoel@gmail.com>
2025-08-02 12:13:09 +0200

    expand tab
    >

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,35 +7,36 @@ import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
-  ],
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
-  base: "https://knnkt.dk",
-  preview: {
-    port: 3000,
-    strictPort: true,
-  },
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-    strictPort: true,
-    hmr: {
-      host: "0.0.0.0",
-      clientPort: 3000,
-    },
-    watch: {
-      usePolling: true,
-    },
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-    },
-  },
+	plugins: [
+		TanStackRouterVite({ autoCodeSplitting: true }),
+		viteReact(),
+		tailwindcss(),
+	],
+	test: {
+		globals: true,
+		environment: "jsdom",
+	},
+	base: "https://knnkt.dk",
+	preview: {
+		port: 3000,
+		strictPort: true,
+	},
+	server: {
+		host: "0.0.0.0",
+		port: 3000,
+		strictPort: true,
+		hmr: {
+			protocol: "ws",
+			host: "localhost",
+			port: 3000,
+		},
+		watch: {
+			usePolling: true,
+		},
+	},
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
+	},
 });
