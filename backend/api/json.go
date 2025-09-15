a1596227e5974fb276614b44cbf0c5b230932582
Mattis Kristensen <mattismoel@gmail.com>
2025-09-16 01:46:12 +0200

    add better error logging for json util functions

--- backend/api/json.go
+++ backend/api/json.go
@@ -2,13 +2,14 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 )
 
 func ReadJSON[T any](r io.Reader, dst *T) error {
 	if err := json.NewDecoder(r).Decode(dst); err != nil {
-		return err
+		return fmt.Errorf("Could not read JSON body: %v", err)
 	}
 
 	return nil
@@ -18,7 +19,7 @@ func WriteJSON[T any](w http.ResponseWriter, v T, status int) error {
 	w.WriteHeader(status)
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(v); err != nil {
-		return err
+		return fmt.Errorf("Could not write JSON: %v", err)
 	}
 
 	return nil
