fe5e0062f1d34d544969c70e9facbc43a008651b
Mattis Kristensen <mattismoel@gmail.com>
2025-09-17 00:08:04 +0200

    add frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { resolve } from 'node:path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [
+		TanStackRouterVite({ autoCodeSplitting: true }),
+		viteReact(),
+		tailwindcss(),
+	],
+	base: import.meta.env.PROD ? "https://knnkt.dk" : "https://localhost",
+	server: {
+		host: "0.0.0.0",
+		port: 3000,
+		strictPort: true,
+		hmr: {
+			clientPort: 443,
+		},
+		watch: {
+			usePolling: true
+		}
+	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+	},
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, './src'),
+		},
+	},
+})
