e6e58c016a9e0075840c1079880b3eed91646d14
Mattis Kristensen <mattismoel@gmail.com>
2025-09-19 13:00:09 +0200

    remove left over debug statement

--- frontend/src/lib/hooks/useClickOutside.ts
+++ frontend/src/lib/hooks/useClickOutside.ts
@@ -5,13 +5,10 @@ export const useClickOutside = <T extends HTMLDivElement>(ref: RefObject<T | nul
 		if (!ref) return
 
 		const handleEvent = (e: MouseEvent) => {
-			console.log("HANDLED EVENT")
 			const target = e.target as Node
 			if (!target || !target.isConnected) return
 
-
 			const isOutside = ref.current && !ref.current.contains(target)
-
 			if (isOutside) {
 				handler(e)
 			}
