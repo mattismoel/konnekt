b8bc18a8d003c5ae7c69475ede8e52fb70265021
Mattis Kristensen <mattismoel@gmail.com>
2025-09-21 16:13:57 +0200

    update button styling for allowing flex icons

--- frontend/src/lib/components/ui/button/base.ts
+++ frontend/src/lib/components/ui/button/base.ts
@@ -7,7 +7,7 @@ export type RootProps = PropsWithChildren<{
 	disabled?: boolean;
 }>
 
-export const baseClasses = "px-5 py-2 rounded-sm font-medium border transition-colors w-full text-nowrap text-center"
+export const baseClasses = "flex justify-center items-center gap-4 px-5 py-2 rounded-sm font-medium border transition-colors w-full text-nowrap text-center"
 
 export const variantClasses = new Map<Variant, string>([
 	["primary", "bg-foreground text-background border-zinc-300 hover:bg-zinc-300 hover:border-zinc-400"],
