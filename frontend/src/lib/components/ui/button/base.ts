064f212e475b3dd4379205b112336feb1daebf1b
Mattis Kristensen <mattismoel@gmail.com>
2025-12-02 01:03:25 +0100

    update button styles
    >

--- frontend/src/lib/components/ui/button/base.ts
+++ frontend/src/lib/components/ui/button/base.ts
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from "react";
 
-type Variant = "primary" | "secondary";
+type Variant = "primary" | "secondary" | "dangerous";
 
 export type RootProps = PropsWithChildren & {
   variant?: Variant;
@@ -8,12 +8,19 @@ export type RootProps = PropsWithChildren & {
 };
 
 export const baseClasses =
-  "flex h-min w-fit items-center justify-center gap-3 rounded-full px-6 py-2 font-medium text-zinc-950 transition-[background,color,filter,border-color] disabled:opacity-50";
+  "flex h-min w-fit items-center border justify-center gap-3 rounded-full px-6 py-2 font-medium text-zinc-950 transition-[background,color,filter,border-color] disabled:opacity-50";
 
 export const variantClasses = new Map<Variant, string>([
-  ["primary", "bg-foreground text-text-dark hover:brightness-65"],
+  [
+    "primary",
+    "bg-foreground text-text-dark border-foreground hover:brightness-65",
+  ],
   [
     "secondary",
-    "text-text-light-muted hover:text-text border border-foreground/10 bg-foreground/5 backdrop-blur-sm hover:border-foreground/30 hover:bg-foreground/10 text-text-light",
+    "text-text-light-muted hover:text-text border-foreground/10 bg-foreground/5 backdrop-blur-sm hover:border-foreground/30 hover:bg-foreground/10 text-text-light",
+  ],
+  [
+    "dangerous",
+    "bg-red-950 border-red-900 text-red-500 hover:border-red-800 hover:bg-red-950 hover:text-red-500 hover:brightness-50",
   ],
 ]);
