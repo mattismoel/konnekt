40ae51da73679b5460a5ba426a2d9a9c56ae55f3
Mattis Kristensen <mattismoel@gmail.com>
2025-12-07 16:47:18 +0100

    update styling of artist list entry

--- frontend/src/lib/features/events/components/EventListEntry.svelte
+++ frontend/src/lib/features/events/components/EventListEntry.svelte
@@ -1,36 +1,48 @@
 <script lang="ts">
-	import type { Artist } from "$lib/features/artist";
-	import type { Event } from "$lib/features/event";
+	import type { Event } from "$lib/features/events/event";
 	import { DATETIME_FORMAT } from "$lib/time";
-	import { format } from "date-fns";
+	import { format, isBefore, startOfToday } from "date-fns";
 
 	type Props = {
 		event: Event;
 	};
 
 	let { event }: Props = $props();
-
-	const artistNames = $derived(event.concerts.map((concert) => concert.artist.name));
 </script>
 
-<li
-	class="group flex items-center rounded-xl border border-zinc-800 bg-zinc-900 transition-colors hover:border-zinc-700 hover:bg-zinc-800"
->
-	<a href="/admin/events/edit/{event.id}" class="w-full py-4 pl-8">
-		<div>
-			<p class="font-medium transition-colors group-hover:text-text-light group-hover:underline">
+<li class="flex flex-col divide-y divide-zinc-800 rounded-2xl border border-zinc-800 bg-zinc-900">
+	<div class="px-8 py-4">
+		<a href="/admin/events/edit/{event.id}" class="hover:underline">
+			<p
+				class={[
+					"mb-2 text-2xl font-bold text-text-light",
+
+					isBefore(event.concerts[0].fromDate, startOfToday()) && "line-through"
+				]}
+			>
 				{event.title}
 			</p>
-			<div class="text-base">
-				<p>{format(event.concerts[0].fromDate, DATETIME_FORMAT)}</p>
-				<p>
-					{#if artistNames.length > 2}
-						{artistNames.slice(0, 2).join(", ")} (+{artistNames.length - 2})
-					{:else}
-						{artistNames.join(", ")}
-					{/if}
-				</p>
-			</div>
-		</div>
-	</a>
+		</a>
+
+		<a href="/admin/venues/edit/{event.venue.id}" class="hover:underline">
+			{event.venue.name}
+		</a>
+
+		<p>{format(event.concerts[0].fromDate, DATETIME_FORMAT)}</p>
+	</div>
+
+	<div class="px-8 py-4">
+		<ul class="flex gap-2">
+			{#each event.concerts as concert}
+				<li class="flex">
+					<a
+						href="/admin/artists/edit/{concert.artist.id}"
+						class="group rounded-full border border-zinc-700 bg-zinc-800 px-6 py-2 text-sm transition-colors hover:border-zinc-600 hover:bg-zinc-700"
+					>
+						{concert.artist.name}
+					</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
 </li>
