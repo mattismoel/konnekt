ecfd9241b894bd2c09d83f2261a1866d7015770a
Mattis Kristensen <mattismoel@gmail.com>
2025-12-07 16:46:59 +0100

    increase padding on artist list entry

--- frontend/src/lib/features/artists/components/ArtistListEntry.svelte
+++ frontend/src/lib/features/artists/components/ArtistListEntry.svelte
@@ -11,7 +11,7 @@
 <li
 	class="rounded-2xl border border-zinc-800 bg-zinc-900 transition-colors hover:border-zinc-700 hover:bg-zinc-800"
 >
-	<a href="/admin/artists/edit/{artist.id}" class="flex flex-col px-8 py-2">
+	<a href="/admin/artists/edit/{artist.id}" class="flex flex-col px-8 py-4">
 		<p class="font-medium text-text-light">{artist.name}</p>
 		<p class="text-sm">{artist.genres.map((genre) => genre.name).join(", ")}</p>
 	</a>
